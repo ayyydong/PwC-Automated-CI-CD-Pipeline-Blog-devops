c90c93e51722f74a6963240d97dab8890315484a
Long (Luke) Nguyen <55564765+LukeLNH@users.noreply.github.com>
2023-02-05 15:02:25 -0800

    PWC-18: Deploy dummy front end in Devops repo to Cloud Run (#2)
    * modify cloudbuild.yaml and trigger build
    
    * modify cloubuild.yaml and trigger build
    
    * add run all react tests command to cloudbuild
    
    * renamed cloudbuild.yaml to non_feature_cloudbuild.yaml
    
    * initial feature cloudbuild.yaml
    
    * Add initial Dockerfile
    
    * update docker file
    
    * WIP update Dockerfile
    
    * WIP update Dockerfile
    
    * WIP
    
    * WIP deploy to cloudrun
    
    * WIP allow unauthenticated access
    
    * testing deployment
    
    * testing deployment
    
    * rename container registry directories
    
    * change non feature cloudbuild to use yarn
    
    * change feature cloudbuild and dockerfile to use yarn
    
    * update node image version

--- dummy-react-app/src/App.js
+++ dummy-react-app/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Testing that a change has been deployed
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
